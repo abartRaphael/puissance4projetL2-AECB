99f4bbdda72c0044c69a4351188fca73465751e6
abartRaphael <Raphael.Abart.Etu@univ-lemans.fr>
2023-04-07 11:05:42 +0200

    fix CU_ASSERT_TRUE typo copier coller

--- version_sdl/test/tests.c
+++ version_sdl/test/tests.c
@@ -452,10 +452,10 @@ void test_quatreALaSuiteDiagonale1(void)
 		grille[LIGNES-4][3].couleur = rouge;
 		grille[LIGNES-3][4].couleur = jauneRouge;
 		grille[LIGNES-2][5].couleur = rouge;
-		CU_ASSERT_FALSE(quatreALaSuiteDiagonale1(grille, LIGNES-5, 2, piece));
-		CU_ASSERT_FALSE(quatreALaSuiteDiagonale1(grille, LIGNES-4, 3, piece));
-		CU_ASSERT_FALSE(quatreALaSuiteDiagonale1(grille, LIGNES-3, 4, piece));
-		CU_ASSERT_FALSE(quatreALaSuiteDiagonale1(grille, LIGNES-2, 5, piece));
+		CU_ASSERT_TRUE(quatreALaSuiteDiagonale1(grille, LIGNES-5, 2, piece));
+		CU_ASSERT_TRUE(quatreALaSuiteDiagonale1(grille, LIGNES-4, 3, piece));
+		CU_ASSERT_TRUE(quatreALaSuiteDiagonale1(grille, LIGNES-3, 4, piece));
+		CU_ASSERT_TRUE(quatreALaSuiteDiagonale1(grille, LIGNES-2, 5, piece));
 	}	
 }
 
