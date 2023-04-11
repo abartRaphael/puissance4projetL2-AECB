6498c6595c5d33c6d84bedcddfc7f3dec448b36f
abartRaphael <Raphael.Abart.Etu@univ-lemans.fr>
2023-04-11 15:34:48 +0200

    création fichier en-tête fonctions menu

---
+++ version_sdl/lib/foncMenu.h
@@ -0,0 +1,22 @@
+#ifndef H_FONCMENU_
+#define H_FONCMENU_
+
+#include <SDL2/SDL.h>
+
+#include "../lib/fonc2.h"
+
+
+/**
+ * \file foncMenu.h
+ * \brief En-tête des fonctions des menus
+ * \version 0.1
+ * \date {11/04/2023}
+ * \author Abart Raphaël
+ * \author ElGhoumari Soumia
+ * \author Coupé Xavier
+*/
+
+
+
+
+#endif /*H_FONCSDL_ */
