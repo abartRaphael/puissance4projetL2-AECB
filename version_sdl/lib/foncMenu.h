96a21bd7a8206fbdc0ed0f5a29ef42c872d19eda
abartRaphael <Raphael.Abart.Etu@univ-lemans.fr>
2023-04-13 10:08:59 +0200

    ajout commentaire pour documentation aux fonctions

--- version_sdl/lib/foncMenu.h
+++ version_sdl/lib/foncMenu.h
@@ -62,11 +62,25 @@ typedef struct rect_menus_s {
 
 
 /**
+ * \fn SDL_Rect creerRect(int x, int y, int w, int h)
+ * \brief crée un "SDL_Rect" à partir de ses 4 champs
+ * \param x coordonnée horizontale du rectangle à partir du bord gauche de la fenêtre
+ * \param y coordonnée verticale du rectangle à partir du bord haut de la fenêtre
+ * \param w longueur du rectangle
+ * \param h hauteur du rectangle
+ * \return retourne un "SDL_Rect"
+ */
+SDL_Rect creerRect(int x, int y, int w, int h);
+
+
+
+/**
  * \fn int initStructTexturesMenu( SDL_Renderer* renderer, images_menus_t* imagesMenus )
  * \brief crée (alloue) les images des menus et les affecte à une structure images_menus_t
  * \param renderer pointeur de SDL_Renderer, nécessaire
  * \param imagesMenus structure contenant des pointeurs sur toutes les textures d'images des menus, à remplir
  * \param typeDePartie type enum, vaut soit modeNormal (1), soit modeCreux (2)
+ * \return retourne 1 pour un succès, 0 pour une erreur
  */
 int initStructTexturesMenu( SDL_Renderer* renderer, images_menus_t* imagesMenus );
 
@@ -100,6 +114,7 @@ void initStructRectMenu( SDL_Renderer* renderer, rect_menus_t* rectMenus, int la
  * \param imagesMenus structure contenant des pointeurs sur toutes les textures d'images des menus
  * \param rectMenus structure contenant les rectangles où placer les images pour les menus
  * \param menu_actuel type enum représentant le menu à afficher
+ * \return 0 pour un succès, -1 pour une erreur
  */
 int afficherMenu( SDL_Renderer* renderer, images_menus_t* imagesMenus, rect_menus_t* rectMenus, affichage_t* menu_actuel );
 
@@ -109,6 +124,12 @@ int gererMenus( SDL_Window* pWindow, SDL_Renderer* renderer, images_menus_t* ima
 
 
 
+/**
+ * \fn Mix_Music* loadMusique(const char* path)
+ * \brief charge une musique à partir d'un fichier
+ * \param path chemin d'accès, relatif ou absolu, vers le fichier de musique (.mp3) à ouvrir
+ * \return retourne un pointeur sur un type "Mix_Music", utilisé pour les fonctions "SDL_mixer"
+ */
 Mix_Music* loadMusique(const char* path);
 
 
--- version_sdl/src/foncMenu.c
+++ version_sdl/src/foncMenu.c
@@ -20,12 +20,15 @@
 
 
 
-
-// La couleur du texte pour les boutons
-// const SDL_Color TEXT_COLOR = {255, 255, 255};
-
-
-
+/**
+ * \fn SDL_Rect creerRect(int x, int y, int w, int h)
+ * \brief crée un "SDL_Rect" à partir de ses 4 champs
+ * \param x coordonnée horizontale du rectangle à partir du bord gauche de la fenêtre
+ * \param y coordonnée verticale du rectangle à partir du bord haut de la fenêtre
+ * \param w longueur du rectangle
+ * \param h hauteur du rectangle
+ * \return retourne un "SDL_Rect"
+ */
 SDL_Rect creerRect(int x, int y, int w, int h){
 	SDL_Rect rectangle;
 	rectangle.x = x ;///Coordonnées x de l'angle haut gauche
@@ -33,13 +36,16 @@ SDL_Rect creerRect(int x, int y, int w, int h){
 	rectangle.w = w ;///Longueur du rectangle
 	rectangle.h = h ;///Hauteur
 	return rectangle ;
-	/**
-	\return rectangle
-	*/
 }
 
 
 
+/**
+ * \fn Mix_Music* loadMusique(const char* path)
+ * \brief charge une musique à partir d'un fichier
+ * \param path chemin d'accès, relatif ou absolu, vers le fichier de musique (.mp3) à ouvrir
+ * \return retourne un pointeur sur un type "Mix_Music", utilisé pour les fonctions "SDL_mixer"
+ */
 Mix_Music* loadMusique(const char* path)
 {
 	// "../musique/musique.mp3"
@@ -49,7 +55,6 @@ Mix_Music* loadMusique(const char* path)
 		printf("Erreur de chargement de la musique : %s\n", Mix_GetError());
 		exit(EXIT_FAILURE);
 	}
-	//Mix_PlayMusic(musique, -1);
 
 	return musique;
 }
@@ -61,6 +66,7 @@ Mix_Music* loadMusique(const char* path)
  * \brief crée (alloue) les images des menus et les affecte à une structure images_menus_t
  * \param renderer pointeur de SDL_Renderer, nécessaire
  * \param imagesMenus structure contenant des pointeurs sur toutes les textures d'images des menus, à remplir
+ * \return retourne 1 pour un succès, 0 pour une erreur
  */
 int initStructTexturesMenu( SDL_Renderer* renderer, images_menus_t* imagesMenus ) {
 	/** 
@@ -90,7 +96,12 @@ int initStructTexturesMenu( SDL_Renderer* renderer, images_menus_t* imagesMenus 
 	imagesMenus->sound_on = loadImage("img/imageMenu/sound_on.bmp",renderer);
 
 		
-	return 0;
+	return(imagesMenus->tache_bleue && imagesMenus->regles && imagesMenus->bouton_back
+		&& imagesMenus->bouton_contre_joueur && imagesMenus->bouton_contre_ordinateur
+		&& imagesMenus->bouton_jouer && imagesMenus->bouton_mode_creux
+		&& imagesMenus->bouton_mode_normal && imagesMenus->bouton_quitter
+		&& imagesMenus->bouton_regles && imagesMenus->bouton_rejouer
+		&& imagesMenus->sound_off && imagesMenus->sound_on);
 }
 
 
@@ -166,6 +177,7 @@ void initStructRectMenu( SDL_Renderer* renderer, rect_menus_t* rectMenus, int la
  * \param imagesMenus structure contenant des pointeurs sur toutes les textures d'images des menus
  * \param rectMenus structure contenant les rectangles où placer les images pour les menus
  * \param menu_actuel type enum représentant le menu à afficher
+ * \return 0 pour un succès, -1 pour une erreur
  */
 int afficherMenu( SDL_Renderer* renderer, images_menus_t* imagesMenus, rect_menus_t* rectMenus, affichage_t* menu_actuel ) {
 
